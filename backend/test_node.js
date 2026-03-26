8b76adac4ae0edd82a8c55ebe0204cbf35dc34db
VIJAYAPANDIANT <vijayapandian112007@gmail.com>
2026-03-26 09:11:37 +0530

    main

--- backend/test_node.js
+++
@@ -1,2 +0,0 @@
-console.log('Test script running');
-setTimeout(() => console.log('Timeout finished'), 1000);
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:8000/api' });
+const API = axios.create({ baseURL: 'http://localhost:8001/api' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('token')) {
