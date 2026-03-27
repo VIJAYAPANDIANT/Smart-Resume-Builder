156d4618e1594510b90ca68402a62c7b643e8f9c
VIJAYAPANDIANT <vijayapandian112007@gmail.com>
2026-03-27 08:53:43 +0530

    main

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:8001/api' });
+const API = axios.create({ baseURL: 'https://smart-resume-builder-one.vercel.app/api' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('token')) {
